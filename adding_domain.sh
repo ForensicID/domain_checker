8418b5c1e6426c82f4be61bfc8604f1d64b272f2
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-09-24 13:43:52 +0700

    Update adding_domain.sh

--- adding_domain.sh
+++ adding_domain.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
 # Konfigurasi
-BOT_TOKEN="7141406407:AAGS79uDhIjYr1JktIG9nr8ncczuztl2wzs"
-CHAT_ID="6422320269"
-FILE_PATH="/root/cekdomain/result_main.txt"
+BOT_TOKEN="YOUR_BOT_TOKEN"
+CHAT_ID="YOUR_CHAT_ID"
+FILE_PATH="/root/cekdomain/domains.txt"
 PROCESSED_FILE="/root/cekdomain/domains_registered.txt"
 
 send_telegram_message() {
     local message="$1"
-    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$message&parse_mode=Markdown"
-}
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$m>}
 
 check_domains() {
-    local line_number=0
     while IFS= read -r line; do
-        ((line_number++))
-        
-        # Abaikan header dan baris ke-2
-        if [[ "$line" =~ ^Domain ]] || [ "$line_number" -eq 2 ]; then
+        # Abaikan komentar
+        if [[ "$line" =~ ^# ]]; then
             continue
         fi
-        
+
+        # Ambil domain dari setiap baris
         domain=$(echo "$line" | awk '{print $1}')
-        
+
         # Cek apakah domain sudah pernah diproses
         if ! grep -q "$domain" "$PROCESSED_FILE"; then
-            message="New domain added📝:%0A*$domain*%0A/fulldomain - to see all domains and wait for any second"
+            message="New domain added📝:%0A*$domain*%0A/fulldomain - to see all domains and wait for"
             send_telegram_message "$message"
             echo "$domain" >> "$PROCESSED_FILE"  # Simpan domain yang sudah diproses
         fi
